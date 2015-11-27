6dfacfa5bb682e2ff91919911b94a891e36e2d2f
Asgenum <asgenum1@gmail.com>
2015-11-27 22:01:31 +0400

    Добавлен функционал для окна опций

--- Remember Me v0.1 beta/src/application/view/ControllerOfOptionsWindow.java
+++ Remember Me v0.1 beta/src/application/view/ControllerOfOptionsWindow.java
@@ -1,5 +1,6 @@
 package application.view;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -45,6 +46,13 @@ public class ControllerOfOptionsWindow {
 				}
 			}
 		});
+
+		choiceBoxHour.setItems(FXCollections.observableArrayList(
+				0, 01, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23));
+		choiceBoxMinutes.setItems(FXCollections.observableArrayList(
+				0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+				31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59));
+
 	}
 
 	public AnchorPane getAnchorPaneOfOptionsWindow() {
